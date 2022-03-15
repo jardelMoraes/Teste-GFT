d542c06e74b56e4be716d8b7bc4be9f42892c2df
Perkles <jardelmoraes@alunos.utfpr.edu.br>
2022-03-15 18:15:39 -0300

    Commit desafio 2

--- Dasafio-2/src/com/impares/Main.java
+++ Dasafio-2/src/com/impares/Main.java
@@ -1,8 +1,31 @@
 package com.impares;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int numero = 0;
+        int v = numero ;
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("digite o numero: ");
+        numero = scan.nextInt();
+
+        while (numero <= 99) {
+            System.out.println("O numero precisa conter 3 digitos\n");
+            System.out.println("digite o numero: ");
+            numero = scan.nextInt();
+
+        }
+
+        if(numero >= 350 && numero <= 8750 ){
+          while (v > 0 ){
+              v = numero/10;
+          }
+
+        }
+
     }
+
 }
